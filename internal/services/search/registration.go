8cba549647fbe11ee3655ee3fcad965e7de5ae30
Lucia Lu <qixialu@hotmail.com>
2026-04-23 15:40:22 +1000

    Add lintignore

--- internal/services/search/registration.go
+++ internal/services/search/registration.go
@@ -53,6 +53,7 @@ func (r Registration) SupportedDataSources() map[string]*pluginsdk.Resource {
 }
 
 // SupportedResources returns the supported Resources supported by this Service
+// lintignore:AZNR005
 func (r Registration) SupportedResources() map[string]*pluginsdk.Resource {
 	return map[string]*pluginsdk.Resource{
 		"azurerm_search_service": resourceSearchService(),
